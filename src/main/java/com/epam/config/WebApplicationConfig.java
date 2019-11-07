eadd7f85a68ca3b11a14067291a8257016331fbe
AleksandraBogdan <aleksandrabogdan@mail.ru>
2019-11-07 17:18:36 +0300

    Fix pom file

--- src/main/java/com/epam/config/WebApplicationConfig.java
+++ src/main/java/com/epam/config/WebApplicationConfig.java
@@ -1,25 +1,33 @@
 package com.epam.config;
 
 import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
 
 
 public class WebApplicationConfig implements WebApplicationInitializer {
 
     @Override
-    public void onStartup(ServletContext servletContext) {
+    public void onStartup(ServletContext servletContext)  {
         AnnotationConfigWebApplicationContext webApplicationContext = new AnnotationConfigWebApplicationContext();
+        //webApplicationContext.scan("com.epam");
         webApplicationContext.register(ApplicationConfig.class);
-        webApplicationContext.refresh();
+
+
+       // servletContext.addListener(new ContextLoaderListener(webApplicationContext));
+        webApplicationContext.setServletContext(servletContext);
+        //webApplicationContext.refresh();
         DispatcherServlet dispatcherServlet = new DispatcherServlet(webApplicationContext);
-        ServletRegistration.Dynamic appServlet = servletContext.addServlet("dispatcherServlet", dispatcherServlet);
+        ServletRegistration.Dynamic appServlet = servletContext.addServlet("mvc", dispatcherServlet);
 
         appServlet.setLoadOnStartup(1);
         appServlet.addMapping("/");
+        System.out.println("Good");
 
     }
 
